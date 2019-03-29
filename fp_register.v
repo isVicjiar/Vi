15957740924bb0e3723ba9cafe9782fe7a1638ea
bscuser <vjimenez>
2019-03-29 12:38:43 +0100

    Add vi_core.v, register.v, alu.v, fp_register.v Completed register.v and fp_regster.v

---
+++ fp_register.v
@@ -0,0 +1,35 @@
+# Floating point register file
+
+module int_registers(
+input		clock,
+input		reset,
+input	[31:0]	write_data,
+input	[4:0]	read_addr_a,	
+input	[4:0]	read_addr_b,
+input	[4:0]	write_addr,
+input		write_enable,
+output	[31:0]	read_data_a,
+output	[31:0]	read_data_b);
+
+reg [31:0] registers [31:0]
+
+# Reset 
+always @(negedge reset)
+begin
+	for (i=0; n<32; i=i+1) registers[i]=0;	
+end
+
+# Read data
+assign read_data_a = registers[read_addr_a];
+assign read_data_b = registers[read_addr_b];
+
+# Write
+always @(clock)
+begin
+	if (clock == 1)
+		if (write_enable == 1)
+			registers[write_addr] <= write_data;
+		end
+	end
+end
+end module	
---
+++ vi_core.v
@@ -0,0 +1,14 @@
+# Main module of the project
+
+module vi_core (
+# Core inputs
+# Core outputs);
+
+wire A;
+
+register register(
+	.clock		(),
+	.reset		(),
+	.write_enable	(),
+	.write_addr	(),
+);
