9dc652a89e771667650cdc562d0d2763bcc65923
isVicjiar <isvicjiar@gmail.com>
2019-11-16 10:46:55 +0100

    Update exe_wb_latch.v

--- exe_wb_latch.v
+++ exe_wb_latch.v
@@ -4,27 +4,21 @@ module exe_wb_latch(
 input		clock,
 input		reset,
 input	[63:0]	exe_int_write_data,
-input	[31:0]	exe_fp_write_data,
 input	[4:0]	exe_write_addr,
 input		exe_int_write_enable,
-input		exe_fp_write_enable,
 input	[31:0]	exe_instruction,
 
 output	[63:0]	wb_int_write_data,
-output	[31:0]	wb_fp_write_data,
 output	[4:0]	wb_write_addr,
 output		wb_int_write_enable,
-output		wb_fp_write_enable,
 output	[31:0]	wb_instruction);
 
 // Reset 
 always @(negedge reset)
 begin
 	wb_int_write_data = 0;
-	wb_fp_write_data = 0;
 	wb_write_addr = 0;
 	wb_int_write_enable = 0;
-	wb_fp_write_enable = 0;
 	wb_instruction = 0;
 end
 
@@ -33,10 +27,8 @@ always @(posedge clock)
 begin
 	if (clock == 1)
 		wb_int_write_data = exe_int_write_data;
-		wb_fp_write_data = exe_fp_write_data;
 		wb_write_addr = exe_write_addr;
 		wb_int_write_enable = exe_int_write_enable;
-		wb_fp_write_enable = exe_fp_write_enable;
 		wb_instruction = exe_instruction;
 end
 endmodule	
