bd7b780ed7a121ffe60ab4ceaa49932e6478d2d9
isVicjiar <isvicjiar@gmail.com>
2019-11-16 10:45:06 +0100

    Update reg_exe_latch.v

--- reg_exe_latch.v
+++ reg_exe_latch.v
@@ -5,19 +5,13 @@ input		clock,
 input		reset,
 input	[63:0]	reg_int_data_a,
 input	[63:0]	reg_int_data_b,
-input	[31:0]	reg_fp_data_a,
-input	[31:0]	reg_fp_data_b,
 input		reg_int_write_enable,
-input		reg_fp_write_enable,
 input	[4:0]	reg_write_addr,
 input	[31:0]	reg_instruction,
 
 output	[63:0]	exe_int_data_a,
 output	[63:0]	exe_int_data_b,
-output	[31:0]	exe_fp_data_a,
-output	[31:0]	exe_fp_data_b,
 output		exe_int_write_enable,
-output		exe_fp_write_enable,
 output	[4:0]	exe_write_addr,
 output	[31:0]	exe_instruction);
 
@@ -26,10 +20,7 @@ always @(negedge reset)
 begin
 	exe_int_data_a = 0;
 	exe_int_data_b = 0;
-	exe_fp_data_a = 0;
-	exe_fp_data_b = 0;
 	exe_int_write_enable = 0;
-	exe_fp_write_enable = 0;
 	exe_write_addr = 0;
 	exe_instruction = 0;
 end
@@ -40,10 +31,7 @@ begin
 	if (clock == 1) 
 		exe_int_data_a = reg_int_data_a;
 		exe_int_data_b = reg_int_data_b;
-		exe_fp_data_a = reg_fp_data_a;
-		exe_fp_data_b = reg_fp_data_b;
 		exe_int_write_enable = reg_int_write_enable;
-		exe_fp_write_enable = reg_fp_write_enable;
 		exe_write_addr = reg_write_addr;
 		exe_instruction = reg_instruction;
 end
