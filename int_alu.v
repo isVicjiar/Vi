e143bb60a07f908f5e46a24af64b7926987470f2
isVicjiar <isvicjiar@gmail.com>
2019-11-20 21:06:40 +0100

    Update int_alu.v

--- int_alu.v
+++ int_alu.v
@@ -1,23 +1,26 @@
 // Integer alu file
 
 module int_alu(
-	input clk_i,
-	input rsn_i,
-	input [31:0] instr_i,
-	input [63:0] data_a_i,
-	input [63:0] data_b_i,
-	output [63:0] data_out_o);
+input clk_i,
+input rsn_i,
+input [31:0] instr_i,
+input [63:0] data_a_i,
+input [63:0] data_b_i,
+output [63:0] data_out_o);
 
-	wire	[7:0]	inst_group;
-wire	[2:0]	inst_type;
+wire	[6:0]	opcode;
+wire	[6:0]	funct7;
+wire	[2:0]	funct3;
 reg	[63:0]	result;
 wire	[63:0]	unsigned_ext_imm;
 wire	[63:0]	ext_imm;
 
-assign	inst_group = instruction[7:0];
-assign	inst_type = instruction[14:12];
-assign	ext_imm = {{52{instruction[31]}}, instruction[31:20]};
-assign	unsigned_ext_imm = {42'b0, instruction[31:20]};
+assign	opcode = instr_i[6:0];
+assign	funct7 = instr_i[31:25];
+assign	funct3 = instr_i[14:12];	
+
+assign	ext_imm = {{52{instr_i[31]}}, instr_i[31:20]};
+assign	unsigned_ext_imm = {52'b0, instr_i[31:20]};
 
 always@(*)
 begin
