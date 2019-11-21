66044a16ff80604088b118c01d23129c7e6173d8
isVicjiar <isvicjiar@gmail.com>
2019-11-21 22:34:14 +0100

    Update int_alu.v

--- int_alu.v
+++ int_alu.v
@@ -3,33 +3,33 @@
 module int_alu(
 input clk_i,
 input rsn_i,
-input [63:0] pc_i,
+input [31:0] pc_i,
 input [31:0] instr_i,
-input [63:0] data_a_i,
-input [63:0] data_b_i,
-output [63:0] data_out_o);
+input [31:0] data_a_i,
+input [31:0] data_b_i,
+output [31:0] data_out_o);
 
 wire	[6:0]	opcode;
 wire	[6:0]	funct7;
 wire	[2:0]	funct3;
 reg	[63:0]	result;
-wire	[63:0]	unsigned_ext_imm;
-wire	[63:0]	ext_imm;
-wire	[63:0] jal_ext_imm;
-wire	[63:0] branch_ext_imm;
-wire	[63:0] store_ext_imm;
-wire	[63:0] load_ext_imm;
+wire	[31:0]	unsigned_ext_imm;
+wire	[31:0]	ext_imm;
+wire	[31:0] jal_ext_imm;
+wire	[31:0] branch_ext_imm;
+wire	[31:0] store_ext_imm;
+wire	[31:0] load_ext_imm;
 
 assign	opcode = instr_i[6:0];
 assign	funct7 = instr_i[31:25];
 assign	funct3 = instr_i[14:12];	
 
-assign	ext_imm = {{52{instr_i[31]}}, instr_i[31:20]};
-assign	unsigned_ext_imm = {52'b0, instr_i[31:20]};
-assign  jal_ext_imm = { 43{instr_i[31]}, instr_i[31], instr_i[19:12], instr_i[20], instr_i[30:21], 1'b0 };
-assign  branch_ext_imm = { 51{instr_i[31]}, instr_i[31], instr_i[7], instr_i[30:25], instr_i[11:8], 1'b0 };
-assign  load_ext_imm = { 52{instr_i[31]}, instr_i[31:25], instr_i[11:7] };
-assign  store_ext_imm = { 52{instr_i[31]}, instr_i[31:20] };
+assign	ext_imm = {{20{instr_i[31]}}, instr_i[31:20]};
+assign	unsigned_ext_imm = {20'b0, instr_i[31:20]};
+assign  jal_ext_imm = { 11{instr_i[31]}, instr_i[31], instr_i[19:12], instr_i[20], instr_i[30:21], 1'b0 };
+assign  branch_ext_imm = { 19{instr_i[31]}, instr_i[31], instr_i[7], instr_i[30:25], instr_i[11:8], 1'b0 };
+assign  load_ext_imm = { 20{instr_i[31]}, instr_i[31:25], instr_i[11:7] };
+assign  store_ext_imm = { 20{instr_i[31]}, instr_i[31:20] };
 assign  data_out_o = result;
 
 always@(*)
