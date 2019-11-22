40c69753e83a10713cdeefdf20977e1436bf2cb3
isVicjiar <isvicjiar@gmail.com>
2019-11-22 21:32:57 +0100

    Update vi_core.v

--- vi_core.v
+++ vi_core.v
@@ -1,11 +1,11 @@
 // Main module of the project
 
 module vi_core (
-input		clock,
-input		reset);
+input		clk_i,
+input		rsn_i);
 
 // TEMPORARY WIRES
-wire [63:0] int_alu_out;
+wire [31:0] int_alu_out;
 
 // Stage instructions
 wire [31:0] fetch_instruction;
@@ -52,59 +52,43 @@ fetch_dec_latch fetch_dec_latch(
 );
 
 decoder decoder(
-	.clock		(clock),
-	.reset		(reset),
-	.instruction	(dec_instruction),
+	.clk_i		(clk_i),
+	.rsn_i		(rsn_i),
+	.instruction_i	(dec_instruction),
 
-	.read_addr_a	(dl_read_addr_a),
-	.read_addr_b	(dl_read_addr_b),
-	.write_addr	(dl_write_addr),
-	.int_write_enable	(dl_int_write_enable)
-);
-
-dec_reg_latch dec_reg_latch(
-	.clock		(clock),
-	.reset		(reset),
-	.dec_read_addr_a	(dl_read_addr_a),
-	.dec_read_addr_b	(dl_read_addr_b),
-	.dec_write_addr		(dl_write_addr),
-	.dec_int_write_enable	(dl_int_write_enable),
-	.dec_instruction	(dec_instruction),
-
-	.reg_read_addr_a	(lr_read_addr_a),
-	.reg_read_addr_b	(lr_read_addr_b),
-	.reg_write_addr		(lr_write_addr),
-	.reg_int_write_enable	(lr_int_write_enable),
-	.reg_instruction	(reg_instruction)
+	.read_addr_a_o	(dec_read_addr_a),
+	.read_addr_b_o	(dec_read_addr_b),
+	.write_addr_o	(dl_write_addr),
+	.int_write_enable_o	(dl_int_write_enable)
 );
 
 int_registers int_registers(
-	.clock		(clock),
-	.reset		(reset),
-	.write_data	(lw_int_write_data),
-	.read_addr_a	(lr_read_addr_a),
-	.read_addr_b	(lr_read_addr_b),
-	.write_addr	(lw_write_addr),
-	.write_enable	(lw_int_write_enable),
+	.clk_i		(clk_i),
+	.rsn_i		(rsn_i),
+	.write_data_i	(lw_int_write_data),
+	.read_addr_a_i	(dec_read_addr_a),
+	.read_addr_b_i	(dec_read_addr_b),
+	.write_addr_i	(lw_write_addr),
+	.write_enable_i	(lw_int_write_enable),
 
-	.read_data_a	(rl_int_data_a),
-	.read_data_b	(rl_int_data_b)
+	.read_data_a_o	(dl_read_data_a),
+	.read_data_b_o	(dl_read_data_b)
 );
+	
+dec_exe_latch dec_exe_latch(
+	.clk_i		(clk_i),
+	.rsn_i		(rsn_i),
+	.dec_read_data_a_i	(dl_read_data_a),
+	.dec_read_addr_b_i	(dl_read_data_b),
+	.dec_write_addr_i	(dl_write_addr),
+	.dec_int_write_enable_i	(dl_int_write_enable),
+	.dec_instruction_i	(dec_instruction),
 
-reg_exe_latch reg_exe_latch(
-	.clock		(clock),
-	.reset		(reset),
-	.reg_int_data_a	(rl_int_data_a),
-	.reg_int_data_b	(rl_int_data_b),
-	.reg_int_write_enable	(rl_int_write_enable),
-	.reg_write_addr		(rl_write_addr),
-	.reg_instruction	(reg_instruction),
-
-	.alu_int_data_a	(la_int_data_a),
-	.alu_int_data_b	(la_int_data_b),
-	.alu_int_write_enable	(la_int_write_enable),
-	.exe_write_addr		(la_write_addr),
-	.exe_instruction 	(exe_instruction)
+	.exe_read_addr_a_o	(le_read_addr_a),
+	.exe_read_addr_b_o	(le_read_addr_b),
+	.exe_write_addr_o	(le_write_addr),
+	.exe_int_write_enable_o	(le_int_write_enable),
+	.exe_instruction_o	(exe_instruction)
 );
 
 int_alu int_alu(
