38d7778f67e8b0ff1d7eb1e65846e17683c5b27d
isVicjiar <isvicjiar@gmail.com>
2019-11-28 11:28:01 +0100

    Update decoder.v

--- decoder.v
+++ decoder.v
@@ -5,33 +5,29 @@ input		clk_i,
 input		rsn_i,
 input	[31:0]	instr_i,
 
-output	[4:0]	read_addr_a_o, //rs1
-output	[4:0]	read_addr_b_o, //rs2
-output	[4:0]	write_addr_o,  //rd
+output	[4:0]	read_addr_a_o,
+output	[4:0]	read_addr_b_o,
+output	[4:0]	write_addr_o,
 output		int_write_enable_o);
-
-assign read_addr_a = instruction[19:15];
-assign read_addr_b = instruction[24:20];
-assign write_addr = instruction[11:7];
-assign rs3 = instruction[31:27];
-assign funct7 = instruction[31:25];
-assign funct3 = instruction[14:12];
-assign opcode = instruction[6:0];
-
-always @(negedge reset)
-begin
-	read_addr_a = 0;
-	read_addr_b = 0;
-	write_addr = 0;
-	rs3 = 0;
-	funct7 = 0;
-	funct3 = 0;
-	opcode = 0;
-
-end
-
-always @(instruction)
-begin
 	
+reg bit int_write_enable;
+
+assign read_addr_a_o = instr_i[19:15];
+assign read_addr_b_o = instr_i[24:20];
+assign write_addr_o = instr_i[11:7];
+assign int_write_enable_o = int_write_enable;
+
+always @(*) begin
+	logic [6:0] funct7;
+	logic [2:0] funct3;
+	logic [6:0] opcode;
+ 	funct7 = instr_i[31:25];
+	funct3 = instr_i[14:12];
+	opcode = instr_i[6:0];
+	if (!rsn_i) int_write_enable = 1'b0;
+	else begin
+		if (branch || store) int_write_enable = 1'b0;
+		else int_write_enable = 1'b1;
+	end
 end
 endmodule
