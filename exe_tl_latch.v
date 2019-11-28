c2ec5fc76b476c1ca0a9a0c7dfbc4aac0aff5ecd
isVicjiar <isvicjiar@gmail.com>
2019-11-28 16:30:37 +0100

    Update exe_tl_latch.v

--- exe_tl_latch.v
+++ exe_tl_latch.v
@@ -1,18 +1,19 @@
 // Execution tl latch
 
 module exe_tl_latch (
-input clk_i,
-input rsn_i,
+input 		clk_i,
+input 		rsn_i,
+input 		stall_core_i,
 input	[31:0]	exe_cache_addr_i,
 input	[4:0]	exe_write_addr_i,
 input		exe_int_write_enable_i,
-input [31:0] exe_store_data_i,
+input 	[31:0] 	exe_store_data_i,
 input	[31:0]	exe_instruction_i,
 input	[31:0]	exe_pc_i,
 output	[31:0]	tl_cache_addr_o,
 output	[4:0]	tl_write_addr_o,
 output		tl_int_write_enable_o,
-input [31:0] tl_store_data_o,
+input 	[31:0] 	tl_store_data_o,
 output	[31:0]	tl_instruction_o,
 output 	[31:0]	tl_pc_o);
 );
@@ -31,12 +32,19 @@ begin
 		tl_cache_addr = 32'b0;
 		tl_write_addr = 5'b0;
 		tl_int_write_enable = 1'b0;
-    tl_store_data = 32'b0;
+    		tl_store_data = 32'b0;
 		tl_instruction = 32'b0;
 		tl_pc = 32'b0;
 	end
 	else begin
-
+		if (!stall_core_i) begin
+			tl_cache_addr = exe_cache_addr_i;
+			tl_write_addr = exe_write_addr_i;
+			tl_int_write_enable = exe_int_write_enable_i;
+    			tl_store_data = exe_store_data_i;
+			tl_instruction = exe_instruction_i;
+			tl_pc = exe_pc_i;
+		end
 	end
 end
 endmodule	
