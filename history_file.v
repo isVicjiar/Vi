e4bcca23fcd8b76c417a1b8d964d43a25d411779
isVicjiar <isvicjiar@gmail.com>
2019-11-28 11:45:29 +0100

    Update history_file.v

--- history_file.v
+++ history_file.v
@@ -1,1 +1,11 @@
 // History file
+
+module history_file (
+input clk_i,
+input rsn_i,
+input [4:0] dec_dest_reg_i,
+input [31:0] miss_addr_i,
+input [31:0] dec_pc_i,
+input [31:0] dec_dest_reg_value_i,
+input [4:0] wb_dest_reg_i,
+input wb_exc_i);
