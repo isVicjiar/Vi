6201ba61d2cbd843d7a8333f71eadbb9e3c068fd
isVicjiar <isvicjiar@gmail.com>
2019-11-28 16:37:42 +0100

    Update exe_tl_latch.v

--- exe_tl_latch.v
+++ exe_tl_latch.v
@@ -10,6 +10,7 @@ input		exe_int_write_enable_i,
 input 	[31:0] 	exe_store_data_i,
 input	[31:0]	exe_instruction_i,
 input	[31:0]	exe_pc_i,
+output		tl_cache_enable_o,
 output	[31:0]	tl_cache_addr_o,
 output	[4:0]	tl_write_addr_o,
 output		tl_int_write_enable_o,
@@ -17,6 +18,13 @@ input 	[31:0] 	tl_store_data_o,
 output	[31:0]	tl_instruction_o,
 output 	[31:0]	tl_pc_o);
 );
+
+reg [31:0] tl_cache_addr;
+reg [4:0] tl_write_addr;
+reg tl_int_write_enable;
+reg [31:0] tl_store_data;
+reg [31:0] tl_instruction;
+reg [31:0] tl_pc;
 
 assign tl_cache_addr_o = tl_cache_addr;
 assign tl_write_addr_o = tl_write_addr;
@@ -38,9 +46,16 @@ begin
 	end
 	else begin
 		if (!stall_core_i) begin
+			if (exe_instruction_i[6:0] == 7'b0100011 || exe_instruction_i[6:0] == 7'b0000011) begin
+				tl_int_write_enable = exe_int_write_enable_i;
+				tl_cache_enable = 1'b1;
+			end
+			else begin
+				tl_int_write_enable = 1'b0;
+				tl_cache_enable = 1'b0;
+			end
 			tl_cache_addr = exe_cache_addr_i;
 			tl_write_addr = exe_write_addr_i;
-			tl_int_write_enable = exe_int_write_enable_i;
     			tl_store_data = exe_store_data_i;
 			tl_instruction = exe_instruction_i;
 			tl_pc = exe_pc_i;
