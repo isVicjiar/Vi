9906ece5fa0007a2e5742a980aa84b493f9b70b2
isVicjiar <isvicjiar@gmail.com>
2019-12-06 19:04:18 +0100

    Update mult4_mult5_latch.v

--- mult4_mult5_latch.v
+++ mult4_mult5_latch.v
@@ -3,6 +3,7 @@
 module mult4_mult5_latch(
 input		clk_i,
 input		rsn_i,
+input		kill_i,
 input	[31:0]	mult4_int_write_data_i,
 input	[4:0]	mult4_write_addr_i,
 input		mult4_int_write_enable_i,
@@ -29,7 +30,7 @@ assign mult5_pc_o = mult5_pc;
 // Latch 
 always @(posedge clock)
 begin
-	if (!rsn_i) begin
+	if (!rsn_i || kill_i) begin
 		mult5_int_write_data = 32'b0;
 		mult5_write_addr = 5'b0;
 		mult5_int_write_enable = 1'b0;
